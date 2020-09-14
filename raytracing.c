ae1747270797969a73ed264c1b0b41b04d603b02
vkuikka <villekuikka1@gmail.com>
2020-09-14 21:49:05 +0300

    fixed plane intersect function and made small modifications to some other functions

--- raytracing.c
+++ raytracing.c
@@ -12,19 +12,24 @@
 
 #include "rt.h"
 
-double		ft_3d_plane(t_objects plane, t_ray r)
+float		ft_3d_plane(t_objects plane, t_ray r)
 {
 	float denom = ft_vector_dot(plane.dir, r.dir); 
-    // if (denom > 0)
+    // if (denom < M_PI)
     // if (denom < 1e-6)
-    if (denom < M_PI)
+    if (fabs(denom) > 1e-6)
 	{
 		float asd[3];
-		asd[0] = r.pos[0] - plane.pos[0];
-		asd[1] = r.pos[1] - plane.pos[1];
-		asd[2] = r.pos[2] - plane.pos[2];
+		// asd[0] = r.pos[0] - plane.pos[0];
+		// asd[1] = r.pos[1] - plane.pos[1];
+		// asd[2] = r.pos[2] - plane.pos[2];
+
+		asd[0] = plane.pos[0] - r.pos[0];
+		asd[1] = plane.pos[1] - r.pos[1];
+		asd[2] = plane.pos[2] - r.pos[2];
+
 		float t = ft_vector_dot(asd, plane.dir) / denom; 
-		if (t > 0)
+		if (t > 0.01)
 			return (t);
 	} 
 	return (0); 
@@ -67,14 +72,14 @@ float		ft_3d_sphere(t_objects sphere, t_ray ray)
 	// return ((-b - sqrt(disc)) / (2.0 * a));
 }
 
-float		ft_choose_obj(t_ray ray, t_objects *obj)
+float		ft_choose_obj(t_ray ray, t_objects obj)
 {
 	// if (type == CONE)
 	// 	hit = ft_3d_cone(world->obj, world->view);
-	if (obj->type == PLANE)
-		return(ft_3d_plane(*obj, ray));
-	else if (obj->type == SPHERE)
-		return(ft_3d_sphere(*obj, ray));
+	if (obj.type == PLANE)
+		return(ft_3d_plane(obj, ray));
+	else if (obj.type == SPHERE)
+		return(ft_3d_sphere(obj, ray));
 	// else if (type == CYLINDER)
 	// 	return(ft_3d_cylinder(world->obj, world->view));
 	ft_error("object not valid yet pls\n");
@@ -93,9 +98,11 @@ float		ft_trace_ray(t_objects *obj, t_objects *lights, t_ray ray, int bounces)
 	hit_obj = NULL;
 	while (obj)
 	{
-		hit = ft_choose_obj(ray, obj);
+		hit = ft_choose_obj(ray, *obj);
 		// if ((hit && hit < tmp) || !tmp && (bounces == 0 || ray.obj_index != obj->index))
-		// if (!(bounces && obj->type == 2))
+		// if (!bounces && obj->type == PLANE && hit)
+		// 	return (hit);
+		// if (!(bounces && obj->type == PLANE))
 		if (((hit && hit < tmp) || !tmp) && ray.obj_index != obj->index)
 		{
 			tmp = hit;
@@ -108,13 +115,23 @@ float		ft_trace_ray(t_objects *obj, t_objects *lights, t_ray ray, int bounces)
 	ray.obj_index = hit_obj->index;
 
 	float res = tmp;
-	ray.normal[0] = res * ray.dir[0] - hit_obj->pos[0];
-	ray.normal[1] = res * ray.dir[1] - hit_obj->pos[1];
-	ray.normal[2] = res * ray.dir[2] - hit_obj->pos[2];
+	if (hit_obj->type == SPHERE)
+	{
+		ray.normal[0] = res * ray.dir[0] - hit_obj->pos[0];
+		ray.normal[1] = res * ray.dir[1] - hit_obj->pos[1];
+		ray.normal[2] = res * ray.dir[2] - hit_obj->pos[2];
 
-	ray.reflect[0] = 2 * ray.normal[0] * (ray.dir[0] * ray.normal[0]);
-	ray.reflect[1] = 2 * ray.normal[1] * (ray.dir[1] * ray.normal[1]);
-	ray.reflect[2] = 2 * ray.normal[2] * (ray.dir[2] * ray.normal[2]);
+		// ray.reflect[0] = 2 * ray.normal[0] * (ray.dir[0] * ray.normal[0]);
+		// ray.reflect[1] = 2 * ray.normal[1] * (ray.dir[1] * ray.normal[1]);
+		// ray.reflect[2] = 2 * ray.normal[2] * (ray.dir[2] * ray.normal[2]);
+	}
+	else if (hit_obj->type == PLANE)
+	{
+		ray.normal[0] = res * ray.dir[0] + hit_obj->dir[0];
+		ray.normal[1] = res * ray.dir[1] + hit_obj->dir[1];
+		ray.normal[2] = res * ray.dir[2] + hit_obj->dir[2];
+	}
+
 
 	ray.pos[0] = res * ray.dir[0];
 	ray.pos[1] = res * ray.dir[1];
@@ -148,10 +165,9 @@ void		ft_render(t_window window, t_world world)
 	int			x;
 	int			y;
 
-	ray_increment_x = world.view->normal[0] / RES_X;
-	ray_increment_y = world.view->normal[0] / RES_Y;
-	x = -1;
-	y = -1;
+	ray_increment_x = 1 / RES_X;
+	ray_increment_y = 1 / RES_Y;
+
 	float res;
 	float tmp;
 
@@ -159,43 +175,61 @@ void		ft_render(t_window window, t_world world)
 	ray.pos[1] = world.view->pos[1];
 	ray.pos[2] = world.view->pos[2];
 
-
 	ray.obj_index = -5;
 
 	ray.dir[0] = world.view->dir[0] - ray_increment_x * RES_X / 2;
-	ray.dir[2] = 1;
+	ray.dir[2] = 0.5;
+
+	x = -1;
 	while (++x < RES_X)
 	{
-		y = 0;
-		// ray.dir[1] = world.view->dir[1];
+		y = -1;
 		ray.dir[1] = world.view->dir[1] - ray_increment_y * RES_Y / 2;
 		while (++y < RES_Y)
 		{
-			res = ft_trace_ray(world.obj, world.lights, ray, 0);
 			world.obj = world.first_obj;
 			world.lights = world.first_light;
-			if (res)
+			res = ft_trace_ray(world.obj, world.lights, ray, 0);
+			if (x == RES_X / 2 && y == RES_Y / 2)
+				printf("%f\n", res);
+			if (res != 0.0)
 			{
-				int r = 15;
-				int g = 15;
-				int b = 15;
+				int r = 0;
+				int g = 0;
+				int b = 0;
 				// if (ray.normal[0] > 0)
 				// 	r = ray.normal[0] * 255;
 				// if (ray.normal[1] > 0)
 				// 	g = ray.normal[1] * 255;
 				// if (ray.normal[2] > 0)
 				// 	b = ray.normal[2] * 255;
-				if (res < 2)
+				int shadow = 0;
+				if (res < 0)
 				{
-					res /= 2;
-					r += 255 - 255 * res;
-					g += 255 - 255 * res;
-					b += 255 - 255 * res;
+					shadow = 1;
+					res *= -1;
+				}
+
+				if (res <= M_PI / 2)
+				{
+					res /= M_PI / 2;
+					if (!shadow)
+					{
+						r = 255 - 255 * res;
+						g = 255 - 255 * res;
+						b = 255 - 255 * res;
+					}
+					else
+					{
+						r = 255 * res;
+						g = 255 * res;
+						b = 255 * res;
+					}
 				}
 				SDL_SetRenderDrawColor(window.SDLrenderer, r, g, b, 255);
 			}
 			else
-				SDL_SetRenderDrawColor(window.SDLrenderer, 10, 10, 10, 255);
+				SDL_SetRenderDrawColor(window.SDLrenderer, 0, 0, 0, 255);
 			SDL_RenderDrawPoint(window.SDLrenderer, x, y);
 
 			ray.dir[1] += ray_increment_y;
